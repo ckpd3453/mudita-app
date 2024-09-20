4c185333e0757a343afebf1b6fbe8d318ba5439f
BL-CKPD <chandrakant.prasad@bridgelabz.com>
2024-09-20 16:00:26 +0530

    [Chandss] ADD: Project structure of mudita app

---
+++ src/main/java/com/samuha/mudita_app/MuditaAppApplication.java
@@ -0,0 +1,13 @@
+package com.samuha.mudita_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuditaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuditaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuha/mudita_app/MuditaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuha.mudita_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuditaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
